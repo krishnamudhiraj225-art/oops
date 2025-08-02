b533036acc94445419acf2b7e8d011a3992fb151
abhilash_30026 <krishnamudhiraj225@gmail.com>
2025-08-02 10:34:29 +0530

    program1

---
+++ implicit.java
@@ -0,0 +1,15 @@
+package project;
+
+public class implicit {
+
+	public static void main(String[] args) {
+		 byte b = 10;
+		    int i;
+		    i=b;
+		    System.out.println(b);
+		    System.out.println(i);
+
+
+	}
+
+}
