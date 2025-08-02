892e407cf778ce7583ea0661e4da2a4c77ae6b0b
abhilash_30026 <krishnamudhiraj225@gmail.com>
2025-08-02 10:41:30 +0530

    program5

---
+++ assignment.java
@@ -0,0 +1,14 @@
+package project;
+
+public class assignment {
+
+	public static void main(String[] args) {
+	    int w,x,y,z;
+	    w = 10;
+	    x = 20;
+	    y = w+x;
+	    z = y;      
+
+	}
+
+}
