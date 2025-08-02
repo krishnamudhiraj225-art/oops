9b9246d316fb4513b2d135062cb8bfc8cebffd99
abhilash_30026 <krishnamudhiraj225@gmail.com>
2025-08-02 11:29:38 +0530

    program8

---
+++ onewayifstatements.java
@@ -0,0 +1,18 @@
+package project;
+
+public class onewayifstatements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+		int area;
+		
+		int PI=2,radius=2;
+		{
+		   area = radius * radius * PI;
+		  System.out.println("The area for the circle of radius " +radius + " is " + area);
+		}
+	}
+
+}
+
+
